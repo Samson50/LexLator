d3dedac9efcd08bf81d86727b5cc52c443bf0fe2
Samson50 <jakeweiss50@gmail.com>
2020-04-16 18:19:03 -0400

    Started work on getting district information

--- app/src/main/java/com/example/legate/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/legate/ui/home/HomeFragment.java
@@ -9,7 +9,9 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -24,12 +26,19 @@ import com.example.legate.utils.ConfigManager;
 public class HomeFragment extends Fragment implements AdapterView.OnItemSelectedListener, View.OnClickListener {
 
     private static final String TAG = "HomeFragment";
+    private static final String CIVIC_API = "https://content.googleapis.com/civicinfo/v2/representatives?";
+    private static final String CIVIC_KEY = "AIzaSyBeG0teS-ls3puepwzK89RmuLI_YVRLURQ";
 
     private ConfigManager configManager;
 
     private HomeViewModel homeViewModel;
     private Button goStateButton;
     private String state;
+    private EditText address;
+    private EditText city;
+    private EditText zipCode;
+
+
     private static boolean updated = false;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -54,6 +63,11 @@ public class HomeFragment extends Fragment implements AdapterView.OnItemSelected
         goStateButton = root.findViewById(R.id.goStateButton);
         goStateButton.setVisibility(View.GONE);
         goStateButton.setOnClickListener(this);
+
+        // Initialize address views
+        address = root.findViewById(R.id.edit_address);
+        city = root.findViewById(R.id.edit_city);
+        zipCode = root.findViewById(R.id.edit_state);
 
         // Update Local Cache
         if (!updated) {
@@ -95,5 +109,34 @@ public class HomeFragment extends Fragment implements AdapterView.OnItemSelected
     @Override
     public void onClick(View v) {
         Navigation.findNavController(v).navigate(R.id.nav_state_list);
+    }
+
+    private class DistrictButton implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            // https://developers.google.com/civic-information/docs/v2
+            // https://developers.google.com/civic-information/
+
+            String queryString = CIVIC_API;
+            String addressString = String.format(
+                    "%s, %s, %s, %s",
+                    address.getText().toString(),
+                    city.getText().toString(),
+                    state,
+                    zipCode.getText().toString()
+            );
+            queryString += "address=" + addressString;
+            queryString += "&includeOffices=false";
+            queryString += "&levels=country";
+            queryString += "key=" + CIVIC_KEY;
+
+            Log.d(TAG, queryString);
+
+            // Hide home fragment and display progress overlay
+
+            // Call query task
+
+        }
     }
 }
