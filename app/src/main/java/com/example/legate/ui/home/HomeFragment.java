f25e853a2042a34099b0b1699a21f4b609253b18
Samson50 <jakeweiss50@gmail.com>
2020-04-23 14:45:14 -0400

    Further developed home fragment  - Added district spinner  - Added number st, nd, rd, th...

--- app/src/main/java/com/example/legate/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/legate/ui/home/HomeFragment.java
@@ -22,15 +22,20 @@ import androidx.preference.PreferenceManager;
 
 import com.example.legate.R;
 import com.example.legate.utils.CacheManager;
+import com.example.legate.utils.StateHelper;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -42,10 +47,14 @@ public class HomeFragment extends Fragment implements AdapterView.OnItemSelected
     private static final String CIVIC_API = "https://content.googleapis.com/civicinfo/v2/representatives?";
     private static final String CIVIC_KEY = "AIzaSyBeG0teS-ls3puepwzK89RmuLI_YVRLURQ";
 
+    private Context context;
     private ViewGroup contentLayout;
     private ViewGroup progressOverlay;
-    private ViewGroup addressLayout;
+    private ViewGroup actionLayout;
+    private Spinner districtSpinner;
+    private ArrayList<String> districtList = new ArrayList<>();
     private String district;
+    private String districtPreference;
     private Button goStateButton;
     private String state;
     private EditText address;
@@ -58,7 +67,7 @@ public class HomeFragment extends Fragment implements AdapterView.OnItemSelected
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        Context context = getActivity();
+        context = getActivity();
         assert context != null;
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
@@ -74,8 +83,11 @@ public class HomeFragment extends Fragment implements AdapterView.OnItemSelected
         stateSpinner.setOnItemSelectedListener(this);
 
         // Hide district & address information
-        addressLayout = root.findViewById(R.id.address_layout);
-        addressLayout.setVisibility(View.GONE);
+        actionLayout = root.findViewById(R.id.spinner_hidden);
+        actionLayout.setVisibility(View.GONE);
+
+        // Initialize District selection spinner
+        districtSpinner = root.findViewById(R.id.district_spinner);
 
         // Initialize goStateButton
         goStateButton = root.findViewById(R.id.goStateButton);
@@ -104,7 +116,7 @@ public class HomeFragment extends Fragment implements AdapterView.OnItemSelected
         if (null != cityValue && cityValue.length() != 0) city.setText(cityValue);
         String zipValue = preferences.getString("zip_preference", null);
         if (null != zipValue && zipValue.length() != 0) zipCode.setText(zipValue);
-        district = preferences.getString("district_preference", null);
+        districtPreference = preferences.getString("district_preference", null);
 
 
         // Update Local Cache
@@ -117,29 +129,106 @@ public class HomeFragment extends Fragment implements AdapterView.OnItemSelected
         return root;
     }
 
+    private void listDistricts() {
+        StateHelper helper = new StateHelper();
+        String shortState = helper.fullToShort(state);
+        File stateFile = new File(context.getCacheDir(), "states/" + shortState);
+        File[] legislators = stateFile.listFiles();
+        if (null == legislators) {
+            Log.e(TAG, "listDistricts(): Failed to list legislators");
+            return;
+        }
+
+        districtList.clear();
+        for (File legislator: legislators) {
+            if (legislator.getName().startsWith("R-")) {
+                String district = legislator.getName().split("-")[1];
+                districtList.add(district);
+            }
+        }
+
+        Collections.sort(districtList, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return Integer.parseInt(o1) - Integer.parseInt(o2);
+            }
+        });
+
+        districtList.add(0, "?");
+
+        ArrayAdapter<String> districtAdapter = new ArrayAdapter<>(context,
+                android.R.layout.simple_spinner_item, districtList);
+        districtSpinner.setAdapter(districtAdapter);
+        districtSpinner.setOnItemSelectedListener(new DistrictSpinnerListener());
+
+        if (null != districtPreference && districtPreference.length() != 0) {
+            int index = districtList.indexOf(districtPreference);
+            if (-1 != index) districtSpinner.setSelection(districtList.indexOf(districtPreference));
+        }
+    }
+
+    private class DistrictSpinnerListener implements AdapterView.OnItemSelectedListener {
+
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            district = parent.getItemAtPosition(position).toString();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+            district = null;
+        }
+    }
+
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         if (position != 0) {
             state = parent.getItemAtPosition(position).toString();
+            listDistricts();
             goStateButton.setVisibility(View.VISIBLE);
-            addressLayout.setVisibility(View.VISIBLE);
+            actionLayout.setVisibility(View.VISIBLE);
         }
         else {
             state = null;
+            districtList.clear();
             goStateButton.setVisibility(View.GONE);
-            addressLayout.setVisibility(View.GONE);
+            actionLayout.setVisibility(View.GONE);
         }
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
         state = null;
+        districtList.clear();
         goStateButton.setVisibility(View.GONE);
+        actionLayout.setVisibility(View.GONE);
+    }
+
+    private String numberFlavor(String district) {
+        int districtLength = district.length();
+        if (2 == districtLength && district.substring(0, 1).equals("1")) return "th";
+        switch (district.substring(districtLength - 1)) {
+            case "1": return "st";
+            case "2": return "nd";
+            case "3": return "rd";
+            default: return "th";
+        }
     }
 
     @Override
     public void onClick(View v) {
         Bundle bundle = new Bundle();
+        if (null != district && !district.equals("?")) {
+            String districtName = String.format(
+                    "%s's %s%s Congressional District",
+                    state,
+                    district,
+                    numberFlavor(district)
+            );
+            // Navigate
+            bundle.putString("district", district);
+            bundle.putString("district-name", districtName);
+        }
         bundle.putString("state", state);
         Navigation.findNavController(v).navigate(R.id.nav_state_list, bundle);
     }
@@ -186,11 +275,11 @@ public class HomeFragment extends Fragment implements AdapterView.OnItemSelected
                 }
             }
             else {
-                // TODO: add "th" etc to district name
                 String districtName = String.format(
-                        "%s's %s Congressional District",
+                        "%s's %s%s Congressional District",
                         state,
-                        district
+                        district,
+                        numberFlavor(district)
                 );
                 // Navigate
                 Bundle bundle = new Bundle();
